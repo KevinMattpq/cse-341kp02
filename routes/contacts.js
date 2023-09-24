6a665f00a354b2114090daaeca90bb1a8708e0cb
KevinMattpq <kevinmpena05@gmail.com>
2023-09-23 20:45:12 -0700

    update

---
+++ routes/contacts.js
@@ -0,0 +1,5 @@
+const express = requiere("express");
+const router = express.Router();
+const contacts = require('../controllers/contacts');
+
+// Here I will call the Functions from contacts inside the Controllers Folder
--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 var express = require('express');
 var app = express();
 const port = process.env.PORT || 3000;
+const MongoClient = require('mongodb').MongoClient;
+const mongodb = require('./db/connect');
 
-// app.use("/",require('./routes'));
+app.use("/",require('./routes'));
 
 app.listen(3000, () => {
     console.log(`Server is running on port ${port}`);
 
 })
+
+mongodb.initDb((err, mongodb) => {
+    if (err) {
+      console.log(err);
+    } else {
+      app.listen(port);
+      console.log(`Connected to DB and listening on ${port}`);
+    }
+  });
