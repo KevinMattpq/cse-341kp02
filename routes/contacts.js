6b19ae2fbcad559acbe98c41b0a4640cdcdcf1a7
KevinMattpq <kevinmpena05@gmail.com>
2023-09-23 21:29:42 -0700

    update

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,5 +1,10 @@
 const express = requiere("express");
 const router = express.Router();
-const contacts = require('../controllers/contacts');
+const contactsFromControllers = require('../controllers/contacts');
 
 // Here I will call the Functions from contacts inside the Controllers Folder
+
+router.get('/', contactsFromControllers.getAll);
+router.get('/:id',contactsFromControllers.getSingle);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 var express = require('express');
-var app = express();
-const port = process.env.PORT || 3000;
-const MongoClient = require('mongodb').MongoClient;
+
+var bodyParser = require('body-parser');
+
+const port = process.env.PORT || 8080;
+
 const mongodb = require('./db/connect');
+var app = express();
 
 app.use("/",require('./routes'));
 
@@ -11,11 +14,19 @@ app.listen(3000, () => {
 
 })
 
+app
+  .use(bodyParser.json())
+  .use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+  })
+  .use('/', require('./routes'));
+
 mongodb.initDb((err, mongodb) => {
-    if (err) {
-      console.log(err);
-    } else {
-      app.listen(port);
-      console.log(`Connected to DB and listening on ${port}`);
-    }
-  });
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port);
+    console.log(`Connected to DB and listening on ${port}`);
+  }
+});
