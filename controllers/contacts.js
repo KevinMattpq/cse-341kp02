bd1720db93ba2abfbc85b7c856585328a722dfe3
KevinMattpq <kevinmpena05@gmail.com>
2023-09-25 22:40:00 -0700

    fixing contacts from the controllers Folder

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,5 +1,5 @@
 const mongodb = require('../db/connect');
-const ObjectId = require('mongodb').ObjectId;
+const{ObjectId}  = require('mongodb');
 
 
 const getAllContacts = async(req, res, next) => {
@@ -10,19 +10,25 @@ const getAllContacts = async(req, res, next) => {
     });
 };
 
-const getSingle = async (req,res,next) => {
-    const userId = new ObjectId(req.params.id);
-    const result = await mongodb
-    .getDb()
-    .db()
-    .collection('contacts')
-    .find({_id: userId});
-    result.toArray().then((lists) => {
+const getSingle = async (req, res, next) => {
+    try {
+      const userId = new ObjectId(req.params.id);
+      const result = await mongodb
+        .getDb()
+        .db()
+        .collection('contacts')
+        .findOne({ _id: userId });
+      if (result) {
         res.setHeader('Content-type', 'application/json');
-        res.status(200).json(lists[0]);
-
-    });
-};
+        res.status(200).json(result);
+      } else {
+        res.status(200).json({ error: 'No Contact found' });
+      }
+    } catch (error) {
+      console.error(error);
+      res.status(200).json({ error: 'server error' });
+    }
+  };
 
 module.exports = { getAllContacts, getSingle };
 
