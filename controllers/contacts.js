779de67177c4fea02ef9b310e8c6411ac63d7ca0
KevinMattpq <kevinmpena05@gmail.com>
2023-09-30 17:46:46 -0700

    Assignment03 on New Brack

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,5 +1,6 @@
 const mongodb = require('../db/connect');
 const{ObjectId}  = require('mongodb');
+const { param } = require('../routes');
 
 
 const getAllContacts = async(req, res, next) => {
@@ -30,5 +31,52 @@ const getSingle = async (req, res, next) => {
     }
   };
 
-module.exports = { getAllContacts, getSingle };
+  // New Functions Personal Assignment 03
+  const createContact = async (req, res, next) =>{
+     const newContact = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      favoriteColor: req.body.favoriteColor,
+      bithday: req.body.birthday
+     }
+      const result = await mongodb.getDb().db().collection('contacts').insertOne(newContact)
+      if (result){
+        res.setHeader('Content-type', 'application/json');
+        res.status(201).json(result);
+      }else{
+        console.log("Error")
+      }
+  }
+
+  const updateContact = async (req, res, next) => {
+    const updatedContact = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      favoriteColor: req.body.favoriteColor,
+      bithday: req.body.birthday
+    }
+    const userId = new ObjectId(req.params.id)
+    const result = await mongodb.getDb().db().collection('contacts').replaceOne({_id: userId},updatedContact);
+    if (result){
+      res.setHeader('Content-type', 'application/json');
+      res.status(204).json(result);
+    }else{
+      console.log("Error")
+    }
+    }
+  
+  const deleteContact = async (req, res, next) =>{
+    const userId = new ObjectId(req.params.id)
+    const result = await mongodb.getDb().db().collection('contacts').deleteOne({_id: userId});
+    if (result){
+      res.setHeader('Content-type', 'application/json');
+      res.status(200).json(result);
+    }else{
+      console.log("Error")
+    }
+  }
+
+module.exports = { getAllContacts, getSingle, createContact, updateContact, deleteContact };
 
--- routes/contacts.js
+++ routes/contacts.js
@@ -7,4 +7,9 @@ const contactsFromControllers = require('../controllers/contacts');
 router.get('/', contactsFromControllers.getAllContacts);
 router.get('/:id',contactsFromControllers.getSingle);
 
+// Personal Assignment 03
+router.post('/', contactsFromControllers.createContact);
+router.put('/:id', contactsFromControllers.updateContact);
+router.delete('/:id', contactsFromControllers.deleteContact);
+
 module.exports = router;
